81ad308ebea16ae88569501c8f2c7e0d390b4de3
kpomak <romka_ne@mail.ru>
2023-04-18 17:04:11 +0300

    add static with whitenoise

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from wsgiref.simple_server import make_server
+from whitenoise import WhiteNoise
 
 from config.core import Rainbow
 from config.settings import SERVER_IP_ADDRESS, SERVER_PORT
@@ -6,6 +7,8 @@ from config.urls import url_patterns, context_gen
 
 
 app = Rainbow(url_patterns, context_gen)
+app = WhiteNoise(app)
+app.add_files('./static', 'static/')
 
 server = make_server(SERVER_IP_ADDRESS, SERVER_PORT, app)
 print(f'Server started on {SERVER_IP_ADDRESS or "0.0.0.0"}:{SERVER_PORT}')
