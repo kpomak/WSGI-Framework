b21700cc715f2d9622927be06462ed35d43bda12
kpomak <romka_ne@mail.ru>
2023-04-19 01:55:13 +0300

    contextgenerator

--- config/generic.py
+++ config/generic.py
@@ -1,18 +1,3 @@
-# from jinja2 import Template
-# from os.path import join
-# from os import getcwd
-
-
-# def render(template_name, folder='templates', **kwargs):
-
-#     file_path = join(getcwd(), folder, template_name)
-
-#     with open(file_path, encoding='utf-8') as f:
-#         template = Template(f.read())
-#     return template.render(**kwargs)
-
-
-
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 def render(template_name, folder='templates', **kwargs):
