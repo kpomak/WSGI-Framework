4de166c578a73a4a8e7a27b47e9cf8cb696998c4
kpomak <romka_ne@mail.ru>
2023-05-11 00:49:00 +0300

    app log fake

--- config/core.py
+++ config/core.py
@@ -36,3 +36,30 @@ class Rainbow:
         code, body = view(request)
         start_response(code, [("Content-Type", "text/html")])
         return [body.encode("utf-8")]
+
+
+class LogRainbow(Rainbow):
+    def __call__(self, environ, start_response):
+        print(
+            f"{environ['REQUEST_METHOD']} {environ['PATH_INFO']} params "
+            f"{params_handler[environ['REQUEST_METHOD']]().get_params(environ)}"
+        )
+        return super().__call__(environ, start_response)
+
+
+class FakeRainbow(Rainbow):
+    def __call__(self, environ, start_response):
+        start_response("200 OK", [("Content-Type", "text/html")])
+        return [b"Hello from Fake"]
+
+
+class RainbowFucktory:
+    rainbows = {
+        "app": Rainbow,
+        "log": LogRainbow,
+        "fake": FakeRainbow,
+    }
+
+    @classmethod
+    def create(cls, rainbow_type, *args):
+        return cls.rainbows[rainbow_type](*args)
