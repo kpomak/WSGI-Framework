107967ad039016cfd46df12e988c35e51c5c7ffe
kpomak <romka_ne@mail.ru>
2023-05-17 01:19:12 +0300

    listview

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,17 @@
 from http import HTTPStatus
 
 from config.generic import render
-from config.utils import Logger, route, debug
-from mainapp.engine import Engine
+from config.utils import route, debug
+from config.views import engine, logger, TemplateView, ListView
 from mainapp.serializers import CourseSerializer
 from mainapp.middleware import EmailNotifier, SmsNotifier
 
-engine = Engine()
-logger = Logger(f"{__name__}")
+
 email_notifier = EmailNotifier()
 sms_notifier = SmsNotifier()
 
 
-class TemplateView:
+class View:
     template_name = "index.html"
 
     def __call__(self, request):
@@ -38,13 +37,12 @@ class ContactsView(TemplateView):
     @debug
     def __call__(self, request):
         if request["method"] == "POST":
-            message = request["params"]
-            logger.log(message)
+            logger.log(request["params"])
         return super().__call__(request)
 
 
 @route("/categories/create/")
-class CreateCategoryView(TemplateView):
+class CreateCategoryView(View):
     template_name = "create_category.html"
 
     @debug
@@ -71,12 +69,12 @@ class CreateCategoryView(TemplateView):
 
 
 @route("/categories/")
-class CategoryListView(TemplateView):
+class CategoryListView(ListView):
     template_name = "category_list.html"
 
 
 @route("/courses/create/")
-class CreateCourseView(TemplateView):
+class CreateCourseView(View):
     template_name = "create_course.html"
 
     @debug
@@ -104,7 +102,7 @@ class CreateCourseView(TemplateView):
 
 
 @route("/courses/")
-class CoursesListView(TemplateView):
+class CoursesListView(ListView):
     template_name = "courses_list.html"
 
 
@@ -122,7 +120,7 @@ class CopyCourseView(TemplateView):
 
 
 @route("/auth/register/")
-class RegisterView(TemplateView):
+class RegisterView(View):
     template_name = "register.html"
 
     @debug
@@ -143,12 +141,12 @@ class RegisterView(TemplateView):
 
 
 @route("/students/")
-class CoursesListView(TemplateView):
+class CoursesListView(ListView):
     template_name = "students_list.html"
 
 
 @route("/students/subscribe/")
-class SubscribeView(TemplateView):
+class SubscribeView(View):
     template_name = "subscribe_course.html"
 
     def __call__(self, request):
