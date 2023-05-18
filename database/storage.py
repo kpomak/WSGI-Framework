22865ef2900c1dc26eba1440e95f5f36b9cb2509
kpomak <romka_ne@mail.ru>
2023-05-18 16:07:35 +0300

    init db

---
+++ database/storage.py
@@ -0,0 +1,18 @@
+from sqlite3 import Connection
+
+
+def init_db(path="db.sqlite3"):
+    sql = """
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username VARCHAR (255) NOT NULL,
+        email VARCHAR (255) UNIQUE,
+        phone INTEGER
+        );
+    """
+    connection = Connection(path)
+    connection.execute(sql)
+
+
+if __name__ == "__main__":
+    init_db()
